68af811a4008f26c967beda8997b2ac7bdcba5a2
RashidmMunir001 <rashid_ntu1081@outlook.com>
2021-02-16 16:45:40 +0500

    succesfully submitted to sr chuck on pyhton for every body

--- p4e/ex4.6.py
+++ p4e/ex4.6.py
@@ -1,20 +1,44 @@
-def computepay(fh,frph):
-    if fh<=40:
-             extpay2=fh*frph
-
-    elif fh>40:
-
-             extpay3=(fh-40)*(1.5*frph)+ 40*frph
-    return extpay3
 
 try:
-    hrs = input("Enter Hours:")
+    hrs = input("Enter Hours :")
     rph=input(" enter rate per hour")
     fh=float(hrs)
     frph=float(rph)
 except:
     print("you have entered string, enter numerics")
     quit()
+def computepay(fh,frph):
+    if fh<=40:
+             pay=fh*frph
 
-p = computepay(45,10.50)
+    elif fh>40:
+
+             extpay=(fh-40)*(1.5*frph)+ 40*frph
+    return extpay
+
+#from now on the program code submitted on py4e debugger by sir chuck,exactly same as above
+p = computepay(fh,frph);
+print("Pay",p)
+
+
+
+try:
+    hrs = input("Enter Hours of work :")
+    rph=input(" enter rate per hour")
+    fh=float(hrs)
+    frph=float(rph)
+except:
+    print("you have entered string, enter numerics")
+    quit()
+def computepay(fh,frph):
+    if fh<=40:
+             pay=fh*frph
+
+    else:
+
+             extpay=(fh-40)*(1.5*frph)+ 40*frph
+    return extpay
+
+
+p = computepay(fh,frph)
 print("Pay",p)
